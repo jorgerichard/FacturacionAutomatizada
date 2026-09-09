08e95ba9213dedf2f6b69cfaa62012c3543b00a1
jorge luis salazar parra <jor.salazarp@duocuc.cl>
2026-09-09 00:43:15 -0300

    feat: add customer repository

---
+++ services/customer-service/src/main/java/com/businessflow/customer/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.businessflow.customer.repository;
+
+import com.businessflow.customer.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+    boolean existsByRut(String rut);
+}
